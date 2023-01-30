0ff4d73eafa440e8b49646a115b05b5575076aa6
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2023-01-30 13:36:52 +0100

    get clients helper added

--- socket/socket-helpers.ts
+++ socket/socket-helpers.ts
@@ -1,5 +1,6 @@
 import { Connection } from "../models/connection.model";
 import { Room } from "../models/room.model";
+import { RoomClient } from "../types";
 
 export const exitRoom = async (socketId: string) => {
     const connection = await Connection.findOne({ socketId });
@@ -7,4 +8,12 @@ export const exitRoom = async (socketId: string) => {
     await Room.findOneAndUpdate({ _id: connection.room }, { $pull: { clients: { socketId } } });
     console.log(`Klient o id ${socketId} został usunięty z pokoju.`);
     await Connection.deleteOne({ socketId });
+};
+
+export const getRoomClients = async (socketId: string): Promise<RoomClient[]> => {
+    const connection = await Connection.findOne({ socketId });
+    if (!connection) return;
+    const room = await Room.findOne({ _id: connection.room });
+    if (!room) return;
+    return room.clients as RoomClient[];
 };
