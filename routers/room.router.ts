c8d311840efb6af03e5dc4261d8f694933468c8a
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2023-01-30 13:36:36 +0100

    handling get vide data

--- routers/room.router.ts
+++ routers/room.router.ts
@@ -1,9 +1,25 @@
 import { Router } from "express";
+import { io } from "..";
 import { Room } from "../models/room.model";
+import { RoomVideoNewPayload } from "../types";
 import { sendSuccessfullResponse } from "../utils/response.util";
 
 export const roomRouter = Router()
+    // Create room
     .post('/', async (req, res) => {
         const { _id } = await Room.create({});
         res.send(sendSuccessfullResponse(_id.toString()));
+    })
+
+    // Pobieranie danych o video
+    .post('/:_id/find-video', async (req, res) => {
+        const { _id } = req.params;
+        const { link } = req.body;
+        const src = link;
+        const room = await Room.findOne({ _id });
+        if (!room) return; // Zwrotka że pokoju już nie ma i przekierowanie na stronę główną
+        const { clients } = room;
+        await Room.findOneAndUpdate({ _id }, { $set: { src } });
+        io.to(clients.map(c => c.socketId)).emit('room-video-new', { src } as RoomVideoNewPayload);
+        res.send('New video has been sent.')
     })
