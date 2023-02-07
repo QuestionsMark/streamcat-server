32edf699436a148444a0eeb393b1d1d8a767d92d
Slawomir Dziurman <g1.slawomir.dziurman@gmail.com>
2023-02-07 12:28:24 +0100

    removing inactive rooms added

--- routers/room.router.ts
+++ routers/room.router.ts
@@ -2,13 +2,23 @@ import { Router } from "express";
 import { io } from "..";
 import { Room } from "../models/room.model";
 import { RoomVideoNewPayload } from "../types";
-import { sendSuccessfullResponse } from "../utils/response.util";
+import { removeRoomJob } from "../utils/cron.util";
+import { sendErrorResponse, sendSuccessfullResponse } from "../utils/response.util";
 
 export const roomRouter = Router()
     // Create room
     .post('/', async (req, res) => {
         const { _id } = await Room.create({});
+        removeRoomJob();
         res.status(201).send(sendSuccessfullResponse(_id.toString()));
+    })
+
+    // Pobieranie pojedynczego pokoju
+    .get('/:roomId', async (req, res) => {
+        const { roomId } = req.params;
+        const room = await Room.findOne({ roomId });
+        if (!room) return res.status(404).send(sendErrorResponse('Room expired.'));
+        res.status(200).send(sendSuccessfullResponse('Successfully joined to the room.'));
     })
 
     // Pobieranie danych o video
